1d80b9dc496a9140d95c26c6247fa8a57688d3f6
guy <tttt870226@163.com>
2020-10-10 14:00:21 +0800

    java的文件操作

--- readResourceFile/ReadJSONFile.java
+++ readResourceFile/ReadJSONFile.java
@@ -3,13 +3,13 @@ package com.sky.shellservice.utils;
 import java.io.*;
 
 public class ReadJSONFile {
-    //读取json文件
+    //读取文件
     public static String readJsonFile(String fileName) {
         String jsonStr = "";
         try {
             File jsonFile = new File(fileName);
             FileReader fileReader = new FileReader(jsonFile);
-            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
+            Reader reader = new InputStreamReader(new FileInputStream(jsonFile), "utf-8");
             int ch = 0;
             StringBuffer sb = new StringBuffer();
             while ((ch = reader.read()) != -1) {
@@ -24,4 +24,109 @@ public class ReadJSONFile {
             return null;
         }
     }
+
+    //写入文件
+    public static int writeNewFile(String filePath, String content) throws Exception {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            } else {
+                DeleteFolder(filePath);
+                file.createNewFile();
+            }
+            //使用true，即进行append file
+            FileWriter fileWritter = new FileWriter(file, true);
+            fileWritter.write(content);
+            fileWritter.flush();
+            fileWritter.close();
+            System.out.println("finish");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return 0;
+    }
+
+
+    /**
+     * 根据路径删除指定的目录或文件，无论存在与否
+     *
+     * @param sPath 要删除的目录或文件
+     * @return 删除成功返回 true，否则返回 false。
+     */
+    public static boolean DeleteFolder(String sPath) {
+        boolean flag = false;
+        File file = new File(sPath);
+        // 判断目录或文件是否存在
+        if (!file.exists()) {  // 不存在返回 false
+            return flag;
+        } else {
+            // 判断是否为文件
+            if (file.isFile()) {  // 为文件时调用删除文件方法
+                return deleteFile(sPath);
+            } else {  // 为目录时调用删除目录方法
+                return deleteDirectory(sPath);
+            }
+        }
+
+
+    }
+
+    /**
+     * 删除单个文件
+     *
+     * @param sPath 被删除文件的文件名
+     * @return 单个文件删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String sPath) {
+        boolean flag = false;
+        File file = new File(sPath);
+        // 路径为文件且不为空则进行删除
+        if (file.isFile() && file.exists()) {
+            file.delete();
+            flag = true;
+        }
+        return flag;
+    }
+
+    /**
+     * 删除目录（文件夹）以及目录下的文件
+     *
+     * @param sPath 被删除目录的文件路径
+     * @return 目录删除成功返回true，否则返回false
+     */
+    public static boolean deleteDirectory(String sPath) {
+        //如果sPath不以文件分隔符结尾，自动添加文件分隔符
+        if (!sPath.endsWith(File.separator)) {
+            sPath = sPath + File.separator;
+        }
+        File dirFile = new File(sPath);
+        //如果dir对应的文件不存在，或者不是一个目录，则退出
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        boolean flag = true;
+        //删除文件夹下的所有文件(包括子目录)
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            //删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag) break;
+            } //删除子目录
+            else {
+                flag = deleteDirectory(files[i].getAbsolutePath());
+                if (!flag) break;
+            }
+        }
+        if (!flag) return false;
+        //删除当前目录
+        if (dirFile.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
