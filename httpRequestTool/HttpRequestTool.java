ce7b1e112f73bc279cfffab712eaa307ee67c91f
guy <tttt870226@163.com>
2020-10-16 09:48:10 +0800

    java的http请求写法总结

---
+++ httpRequestTool/HttpRequestTool.java
@@ -0,0 +1,216 @@
+package com.sky.shellservice.utils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 需要的maven的jar包
+ <dependency>
+ <groupId>org.apache.httpcomponents</groupId>
+ <artifactId>httpclient</artifactId>
+ <version>4.3</version>
+ </dependency>
+ <dependency>
+ <groupId>org.apache.httpcomponents</groupId>
+ <artifactId>httpmime</artifactId>
+ <version>4.3</version>
+ </dependency>
+
+ <dependency>
+ <groupId>org.apache.commons</groupId>
+ <artifactId>commons-lang3</artifactId>
+ <version>3.1</version>
+ </dependency>
+
+ */
+
+public class HttpRequestTool {
+
+    /**
+     * get请求
+     * @param httpurl
+     * @return
+     */
+    public static String doGet(String httpurl) {
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        BufferedReader br = null;
+        String result = null;// 返回结果字符串
+        try {
+            // 创建远程url连接对象
+            URL url = new URL(httpurl);
+            // 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置连接方式：get
+            connection.setRequestMethod("GET");
+            // 设置连接主机服务器的超时时间：15000毫秒
+            connection.setConnectTimeout(15000);
+            // 设置读取远程返回的数据时间：60000毫秒
+            connection.setReadTimeout(60000);
+            // 发送请求
+            connection.connect();
+            // 通过connection连接，获取输入流
+            if (connection.getResponseCode() == 200) {
+                is = connection.getInputStream();
+                // 封装输入流is，并指定字符集
+                br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                // 存放数据
+                StringBuffer sbf = new StringBuffer();
+                String temp = null;
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            connection.disconnect();// 关闭远程连接
+        }
+
+        return result;
+    }
+
+    /**
+     * POST请求 application/json
+     * @param requestUrl
+     * @param Authorization
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    public static String sendPost(String requestUrl,String Authorization,String params) throws Exception {
+        String generalUrl = requestUrl;
+        URL url = new URL(generalUrl);
+        // 打开和URL之间的连接
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        // 设置通用的请求属性
+        connection.setRequestProperty("Content-Type", "application/json");
+        if (StringUtils.isNotBlank(Authorization)){
+            connection.setRequestProperty("Authorization",Authorization);
+        }
+        connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setUseCaches(false);
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        // 得到请求的输出流对象
+        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+        out.write(params.getBytes("utf-8"));
+        out.flush();
+        out.close();
+        // 建立实际的连接
+        connection.connect();
+        // 获取所有响应头字段
+        Map<String, List<String>> headers = connection.getHeaderFields();
+        // 遍历所有的响应头字段
+        for (String key : headers.keySet()) {
+            System.out.println(key + "--->" + headers.get(key));
+        }
+        // 定义 BufferedReader输入流来读取URL的响应
+        BufferedReader in = null;
+        if (requestUrl.contains("nlp")) {
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        }
+        else {
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        }
+        String result = "";
+        String getLine;
+        while ((getLine = in.readLine()) != null) {
+            result += getLine;
+        }
+        in.close();
+        System.out.println("result:" + result);
+        return result;
+    }
+
+    /**
+     * 使用httpclint 发送文件
+     *
+     * @param url:           接口全路径
+     * @param file:          上传文件
+     * @param fileParamName: 接口对应文件的参数名:相当于@RequestParam("fileParamName")
+     * @param headerParams:  请求头信息: 可能需要携带token,注意不要设置content-type
+     * @param otherParams:   其他参数
+     * @return
+     */
+    public static String uploadApiFile(String url, MultipartFile file, String fileParamName, Map<String, String> headerParams, Map<String, String> otherParams) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String result = "";
+        try {
+            String fileName = file.getOriginalFilename();
+            HttpPost httpPost = new HttpPost(url);
+            //添加header
+            for (Map.Entry<String, String> e : headerParams.entrySet()) {
+                httpPost.addHeader(e.getKey(), e.getValue());
+            }
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setCharset(Charset.forName("utf-8"));
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);//加上此行代码解决返回中文乱码问题
+            builder.addBinaryBody(fileParamName, file.getInputStream(), ContentType.MULTIPART_FORM_DATA, fileName);// 文件流
+            for (Map.Entry<String, String> e : otherParams.entrySet()) {
+                builder.addTextBody(e.getKey(), e.getValue());// 类似浏览器表单提交，对应input的name和value
+            }
+            HttpEntity entity = builder.build();
+            httpPost.setEntity(entity);
+            HttpResponse response = httpClient.execute(httpPost);// 执行提交
+            HttpEntity responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                // 将响应内容转换为字符串
+                result = EntityUtils.toString(responseEntity, Charset.forName("UTF-8"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = e.getMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = e.getMessage();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                result = e.getMessage();
+            }
+        }
+        return result;
+    }
+
+}
